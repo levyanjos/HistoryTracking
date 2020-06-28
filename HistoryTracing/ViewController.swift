029e7b5b4ef8c05ac24422e9acc73b6bdae05fdc
Levy cristian <jolevy.cristian@gmail.com>
2020-06-28 20:38:04 -0300

    Initial Commit

---
+++ HistoryTracing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HistoryTracing
+//
+//  Created by Levy Cristian on 28/06/20.
+//  Copyright © 2020 Levy Cristian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
